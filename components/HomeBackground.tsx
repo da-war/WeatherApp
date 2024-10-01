3739bf39610c70d34b6fad159ef9af7d6bea49ea
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-01 12:56:29 +0500

    ImageBackground at home page

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,19 +1,36 @@
-import { StyleSheet, Text, useWindowDimensions, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import React from "react";
 import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
 
 const HomeBackground = () => {
   const { width, height } = useWindowDimensions();
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Rect x={0} y={0} width={width} height={height}>
-        <LinearGradient
-          start={vec(0, 0)}
-          end={vec(width, height)}
-          colors={["#2E335A", "#1C1B33"]}
-        />
-      </Rect>
-    </Canvas>
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={["#2E335A", "#1C1B33"]}
+          />
+        </Rect>
+      </Canvas>
+      <ImageBackground
+        source={require("../assets/home/Background.png")}
+        resizeMode="cover"
+        style={{
+          height: "100%",
+        }}
+      >
+        <Text>Inside</Text>
+      </ImageBackground>
+    </>
   );
 };
 
