2d1107f343c86a2bb4e01b7ccc78e8c5d698e901
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-02 11:55:48 +0500

    Basic animation of a squaure

--- app/BasicAnimation.tsx
+++ app/BasicAnimation.tsx
@@ -1,10 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import Animated, {
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+} from "react-native-reanimated";
 
 const BasicAnimation = () => {
+  const Size = 200;
+  const scale = useSharedValue(1);
+  const borderRadius = useSharedValue(0);
+
+  const squareCircleStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
+  useEffect(() => {
+    scale.value = withRepeat(withSpring(2), -1, true);
+  }, []);
   return (
-    <View>
-      <Text>BasicAnimation</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Animated.View
+        style={[
+          { width: Size, height: Size, backgroundColor: "red" },
+          squareCircleStyles,
+        ]}
+      ></Animated.View>
     </View>
   );
 };
